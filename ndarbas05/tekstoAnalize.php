d354291aa115471fdcbd4fa33dc5fcd257dd5db6
Romanas Chomskis <romanas.chomskis@gmail.com>
2018-02-19 14:13:43 +0200

    --Namu darbas Nr05. v.3.2

--- ndarbas05/tekstoAnalize.php
+++ ndarbas05/tekstoAnalize.php
@@ -1,7 +1,7 @@
 <?php
     $vardas = 'Mindaugas'; // suskaiciuoti balses
 
-    echo '<h4>Balsių paieška tekste</h4>';
+    echo '<h4>Balsių paieška tekste. v.1 naudojant <code><em>array_intersect()</em></code></h4>';
 
     echo 'Išeities tekstas: ' . '<code><em>' . $vardas .  '</em></code>' . '<br>' . '<br>';
     $vMas = str_split(strtoupper($vardas));
@@ -14,4 +14,12 @@
         echo 'Balsės pozicija: ' . $k . ', balsė: ' . $val . '.' . '<br>';
     }
 
+    echo '<br><br>';
+    echo '<h4>Balsių paieška tekste. v.2 naudojant <code><em>substr_count()</em></code></h4>';
+
+    $c = 0;
+    foreach ($vow as $val) {
+        $c += substr_count(strtoupper($vardas), $val);
+    }
+    echo 'Balsiu tekste: ' . $c .'<br>';
 ?>
