4db5f020ab4582d02ee6d5f7276aa60737da00b2
Romanas Chomskis <romanas.chomskis@gmail.com>
2018-02-20 18:21:20 +0200

    --Namu darbas Nr06. v.7.2 Pataisyta: ismestas if, funkcja grazina boolean

--- ndarbas06/tobuloRadimas.php
+++ ndarbas06/tobuloRadimas.php
@@ -1,14 +1,10 @@
 <?php
     function dalikliuRadimas($s) {
-        $dal[] = NULL;
+        $dal =[];
         for ($i = 1; $i < $s; $i++) {
             if ($s % $i === 0) {
-                if ($dal[0] === NULL) {
-                    $dal[0] = $i;
-                }
-                else {
                     $dal[] = $i;
-                }
+
             }
         }
         return $dal;
@@ -18,10 +14,10 @@
 
         $dal = dalikliuRadimas($s);
         if (array_sum($dal) === $s) {
-          return $dal;
+          return true;
         }
         else {
-            return NULL;
+            return false;
         }
     }
 
@@ -29,7 +25,7 @@
 
     echo 'Tobulų skaičių eilė: ';
     for ($i = 1; $i < 1001; $i++) {
-        if (count(tobulaSkaicius($i)) > 0) {
+        if (tobulaSkaicius($i)) {
             echo $i . ', ';
         }
     }
