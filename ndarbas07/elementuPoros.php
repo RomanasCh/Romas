dd5998ea5e2e6697db4a3c40b56b44c37393f33e
Romanas Chomskis <romanas.chomskis@gmail.com>
2018-02-21 14:57:28 +0200

    --Namų darbas Nr07. Užduotis namuose Nr01 v.2

--- ndarbas07/elementuPoros.php
+++ ndarbas07/elementuPoros.php
@@ -1,9 +1,15 @@
 <?php
-    function elemPoros($mas) {
+    function elemPoros($mas, $u) {
 
         $l = count($mas);
         for ($j = 0; $j < $l; $j++) {
-            for ($i = $j; $i < $l; $i++) {
+
+           $p = 0;
+           if ($u) {
+                $p =$j;
+            }
+
+            for ($i = $p; $i < $l; $i++) {
                 if ($i !== $j) {
                     echo $mas[$j] . ' & ' . $mas[$i] . ", "  ;
                 }
@@ -14,8 +20,14 @@
     $a = ['Jonas', 'Petras', 'Antanas','Povilas'];
     
     echo '<h4>Masyvo elementų unikalių porų radimas</h4>';
+    echo 'Išeities masyvas: ' . implode(", ", $a) . '<br><br>';
     echo 'Porų seka: ';
-    elemPoros($a);
+    elemPoros($a, true);
     echo '<br>';
+    echo '<h4>Masyvo elementų vsų įmanomų porų radimas</h4>';
+    echo 'Porų seka: ';
+    elemPoros($a, false);
+    echo '<br>';
+
 ?>
 
