68cf72efb57ca241b9a0c6a398ad6a3e658d796f
Romanas Chomskis <romanas.chomskis@gmail.com>
2018-02-22 16:41:28 +0200

    --Namu darbas Nr08. Uzdavinys namuose Nr01

--- ndarbas08/index.php
+++ ndarbas08/index.php
@@ -1,10 +1,4 @@
 <?php
-require 'zmogausAmzius.php';
-require 'duomenysZm..php';
-
-    echo '<h4>Asmens amžius</h4>';
-    foreach ($zmones as $mas) {
-        echo $mas['vardas'] . ' - ' . zmogAmzius($mas) . ' m. <br>';
-    }
+require 'vyruMoteruPoros.php';
 
 ?>
