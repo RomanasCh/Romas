36ccf42acd8866f7b13be1deb344681ff2ece2fc
Romanas Chomskis <romanas.chomskis@gmail.com>
2018-02-25 20:43:18 +0200

    --Namu darbas Nr10. Uzdavinys namuose Nr01.Pataisyta pagal uzduoti

--- ndarbas10/Mokinys.php
+++ ndarbas10/Mokinys.php
@@ -5,17 +5,18 @@
  * Date: 2018-02-22
  * Time: 20:34
  */
-require 'zmogus.php';
+require 'Trimestras.php';
 
-class Mokinys extends Zmogus
+class Mokinys extends Trimestras
 {
-    public $trimestras;
     public $lygis;
     public function __construct($trimestras, $lygis ,$vardas, $pavarde)
     {
-        parent::__construct($vardas, $pavarde);
+        parent::__construct($trimestras);
+
         $this->lygis = $lygis;
-        $this->trimestras = $trimestras;
+        $this->vardas = $vardas;
+        $this->pavarde = $pavarde;
     }
     public function vidurkis() {
         $rez = 0;
---
+++ ndarbas10/Trimestras.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romas
+ * Date: 2018-02-25
+ * Time: 19:13
+ */
+
+class Trimestras
+{
+    public $trimestras;
+
+    public function __construct($trimestras)
+    {
+        $this->trimestras = $trimestras;
+    }
+}
