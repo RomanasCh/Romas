83e4a4264251b66521c502443149a9ce8359e549
Romanas Chomskis <romanas.chomskis@gmail.com>
2018-02-25 19:06:54 +0200

    --Namu darbas Nr10. Uzdavinys namuose Nr01

--- ndarbas10/index.php
+++ ndarbas10/index.php
@@ -1,2 +1,2 @@
 <?php
-require 'startMokinys.php';
+require 'startMokinys2.php';
---
+++ ndarbas10/startMokinys2.php
@@ -0,0 +1,47 @@
+<?php
+require 'Mokinys.php';
+$mokiniai = [
+    new Mokinys(['lietuviu' => 8, 'matematika' => 9, 'anglu' => 8], '3a', 'Jonas', 'Jonaitis'),
+    new Mokinys(['lietuviu' => 9, 'matematika' => 10, 'anglu' => 10], '2b', 'Ona', 'Onaitė'),
+    new Mokinys(['lietuviu' => 8, 'matematika' => 7, 'anglu' => 9], '2c', 'Antanas', 'Antanaitis'),
+    new Mokinys(['lietuviu' => 9, 'matematika' => 9, 'anglu' => 10], '2b', 'Eglė', 'Eglytė'),
+];
+
+echo '<h4>Pažymių vidurkiai, mžėjimo tvarka</h4>';
+
+$rezult = [];
+$i=0;
+foreach ($mokiniai as $mokinys) {
+    $rezult[$i] = [round($mokinys->vidurkis(),2),$mokinys->vardas, $mokinys->pavarde];
+    $i++;
+}
+array_multisort($rezult,SORT_DESC);
+?>
+<html>
+<body>
+<table>
+    <thead>
+        <tr>
+            <th>Vardas</th>
+            <th>Pavardė</th>
+            <th>Vidurkis</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($rezult as $rz) { ?>
+        <tr>
+            <td>
+                <?= $rz[1] ?>
+            </td>
+            <td>
+                <?= $rz[2] ?>
+            </td>
+            <td>
+                <?= $rz[0] ?>
+            </td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
+</body>
+</html>
