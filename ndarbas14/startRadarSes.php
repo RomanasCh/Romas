db5fcf299461bc1b018c628df5158499474aa0bc
Romanas Chomskis <romanas.chomskis@gmail.com>
2018-03-05 11:35:59 +0200

    Namu darbas Nr14. Pataisymai pagal pastabas

--- ndarbas14/startRadarSes.php
+++ ndarbas14/startRadarSes.php
@@ -1,8 +1,5 @@
 <?php
 
-function masRikiuokLaika ($a, $b){
-    return ($a->date->getTimestamp() < $b->date->getTimestamp()) ? -1 : 1;
-}
 function masRikiuokGreiti ($a, $b){
     return ($a->duokGreiti() < $b->duokGreiti()) ? 1 : -1;
 }
